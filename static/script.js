7f40de3d6d50fed40c2290a8331dc89b8ceaea67
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2024-12-14 00:39:28 +0530

    commit

---
+++ static/script.js
@@ -0,0 +1,21 @@
+// Script for Image Slider
+function nextSlide(button) {
+    const slider = button.parentElement;
+    const images = slider.querySelectorAll('img');
+    let activeIndex = [...images].findIndex(img => img.classList.contains('active'));
+  
+    images[activeIndex].classList.remove('active');
+    activeIndex = (activeIndex + 1) % images.length;
+    images[activeIndex].classList.add('active');
+  }
+  
+  function prevSlide(button) {
+    const slider = button.parentElement;
+    const images = slider.querySelectorAll('img');
+    let activeIndex = [...images].findIndex(img => img.classList.contains('active'));
+  
+    images[activeIndex].classList.remove('active');
+    activeIndex = (activeIndex - 1 + images.length) % images.length;
+    images[activeIndex].classList.add('active');
+  }
+  
